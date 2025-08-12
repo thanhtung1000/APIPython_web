e9c219a0efdae28ffd8349ae073988926291f41b
thanhtung1000 <125711134+thanhtung1000@users.noreply.github.com>
2025-08-12 08:26:18 +0700

    Delete student_manager_web/haha.py

--- student_manager_web/haha.py
+++
@@ -1,7 +0,0 @@
-import pyodbc
-
-conn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};SERVER=LAPTOP-L3PL7A87;DATABASE=danhsachsinhvien;Trusted_Connection=yes")
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM Sinhvien")
-rows = cursor.fetchall()
-print(rows)
